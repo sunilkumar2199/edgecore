697a15944e8e202115e5fdd103c13696edf5f95d
sunilkumar2199 <91337127+sunilkumar2199@users.noreply.github.com>
2026-03-25 21:08:22 +0530

    Update herotext.js

--- blocks/herotext/herotext.js
+++ blocks/herotext/herotext.js
@@ -1,6 +1,7 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
+  console.log("Block = " + block);
   const wrapper = block.querySelector('div > div');
   if (!wrapper) return;
 
