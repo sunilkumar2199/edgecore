59d430fd5a637c6de832494807cebff1817de41a
sunilkumar2199 <91337127+sunilkumar2199@users.noreply.github.com>
2026-03-25 17:06:12 +0530

    Remove herotext.js functionality

--- blocks/herotext/herotext.js
+++ blocks/herotext/herotext.js
@@ -1,13 +1,1 @@
-export default function decorate(block) {
-  const wrapper = block.querySelector('div > div');
-  if (!wrapper) return;
 
-  const paragraphs = wrapper.querySelectorAll('p');
-
-  if (paragraphs.length > 0) {
-    const heading = document.createElement('h1');
-    heading.innerHTML = paragraphs[0].innerHTML;
-
-    wrapper.replaceChild(heading, paragraphs[0]);
-  }
-}
